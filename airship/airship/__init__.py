b8f1cca35bdf296f34c41bd0a87fef3da35a59eb
Carlos Liam <carlos@aarzee.me>
2015-05-17 19:24:42 -0400

    v1.3.2: minor change to base package structure

--- airship/airship/__init__.py
+++ airship/airship/__init__.py
@@ -1,5 +1,5 @@
 import os
-import airship
+from airship import sync
 
 class suppress_stdout_stderr(object): # http://stackoverflow.com/questions/11130156
     def __init__(self):
@@ -7,15 +7,15 @@ class suppress_stdout_stderr(object): # http://stackoverflow.com/questions/11130
         self.save_fds = (os.dup(1), os.dup(2))
 
     def __enter__(self):
-        os.dup2(self.null_fds[0],1)
-        os.dup2(self.null_fds[1],2)
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
 
     def __exit__(self, *_):
-        os.dup2(self.save_fds[0],1)
-        os.dup2(self.save_fds[1],2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
         os.close(self.null_fds[0])
         os.close(self.null_fds[1])
 
 def main():
     with suppress_stdout_stderr():
-        airship.sync()
+        sync()
